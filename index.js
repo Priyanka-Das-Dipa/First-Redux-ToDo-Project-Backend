c4a2a90dcf0db7332fab3f0706e9f295af3251a1
Priyanka Das Dipa <priyankadipa9102@gmail.com>
2024-07-03 01:20:18 +0600

    todo backend done

---
+++ index.js
@@ -0,0 +1,80 @@
+require("dotenv").config();
+const express = require("express");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const app = express();
+const port = process.env.PORT || 5000;
+
+const cors = require("cors");
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb://localhost:27017`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+const run = async () => {
+  try {
+    const db = client.db("todo");
+    const taskCollection = db.collection("tasks");
+    // get the data from the DB
+    app.get("/tasks", async (req, res) => {
+      const cursor = taskCollection.find({});
+      const tasks = await cursor.toArray();
+      res.send({ status: true, data: tasks });
+    });
+
+    app.post("/task", async (req, res) => {
+      const task = req.body;
+      const result = await taskCollection.insertOne(task);
+      res.send(result);
+    });
+
+    app.get("/task/:id", async (req, res) => {
+      const id = req.params.id;
+      const result = await taskCollection.findOne({ _id: ObjectId(id) });
+      // console.log(result);
+      res.send(result);
+    });
+
+    app.delete("/task/:id", async (req, res) => {
+      const id = req.params.id;
+      const result = await taskCollection.deleteOne({ _id: ObjectId(id) });
+      // console.log(result);
+      res.send(result);
+    });
+
+    // status update
+    app.put("/task/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const task = req.body;
+      const filter = { _id: ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          isCompleted: task.isCompleted,
+          title: task.title,
+          description: task.description,
+          priority: task.priority,
+        },
+      };
+      const options = { upsert: true };
+      const result = await taskCollection.updateOne(filter, updateDoc, options);
+      res.json(result);
+    });
+  } finally {
+  }
+};
+
+run().catch((err) => console.log(err));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
